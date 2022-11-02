68763dea32413ad02fec186660fb1356b7d6b984
Admin <shaidurov.ilha@yandex.ru>
2022-11-02 15:26:24 +0100

    3. PostController.createPost. Создание вакансии. [#504849]

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -3,6 +3,8 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
 
@@ -21,8 +23,14 @@ public class PostController {
 
     @GetMapping("/formAddPost")
     public String addPost(Model model) {
-        model.addAttribute("post", new Post(0, "Заполните название",
-                "Заполните описание", LocalDateTime.now()));
+        model.addAttribute("post", new Post(0, "Заполните поле",
+                "Заполните поле", LocalDateTime.now()));
         return "addPost";
+    }
+
+    @PostMapping("/createdPost")
+    public String createPost(@ModelAttribute Post post) {
+        store.add(post);
+        return "redirect:/posts";
     }
 }
--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -6,17 +6,17 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PostStore {
 
     private static final PostStore INST = new PostStore();
 
+    private final AtomicInteger idS = new AtomicInteger(1);
+
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job", "CCC", LocalDateTime.now()));
-        posts.put(2, new Post(2, "Middle Java Job", "BBB", LocalDateTime.now()));
-        posts.put(3, new Post(3, "Senior Java Job", "AAA", LocalDateTime.now()));
     }
 
     public static PostStore instOf() {
@@ -25,5 +25,11 @@ public class PostStore {
 
     public Collection<Post> findAll() {
         return posts.values();
+    }
+
+    public void add(Post post) {
+        post.setId(idS.getAndIncrement());
+        post.setCreated(LocalDateTime.now());
+        posts.put(post.getId(), post);
     }
 }
