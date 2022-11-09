ac2103179e3d751bbe8d390ab80df78e1be18826
Admin <shaidurov.ilha@yandex.ru>
2022-11-09 10:31:53 +0100

    4. Многопоточность в базе данных [#504860]

---
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -0,0 +1,9 @@
+package ru.job4j.dreamjob.controller;
+
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+
+@ThreadSafe
+@Controller
+public class UserController {
+}
